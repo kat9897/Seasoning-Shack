483e0abb7f6d81c33269b7c02b3bcd018a13b3ca
kat9897 <61071011+kat9897@users.noreply.github.com>
2020-02-16 06:35:08 -0500

    Add files via upload

--- search.php
+++ search.php
@@ -15,11 +15,18 @@ if(ISSET($_POST['submitButton'])) {
 		margin:0;
 		padding: 0;
 	}
+	body {
+		background-image: url('search.jpg');
+		background-position: center;
+		background-repeat: no-repeat;
+		background-attachment: fixed;
+		background-size: cover;
+	}
 	.text{
 		color: white;
 		position: absolute;
 		top:8%;
-		left:12%;
+		left:2%;
 		font-size: 35px;
 		font-family: "Arial Black";
 	}
@@ -28,33 +35,38 @@ if(ISSET($_POST['submitButton'])) {
 				height: 100%;
 	}
 	.button{
-		position: absolute;
 		top:90%;
 		left:10%;
+	}
+	.recipe {
+		font-size: 15px;
+		top: 20%;
+		left: 20%;
 	}
 	</style>
 </head>
 <body>
 <div class = "container">
-	<img src = "search.jpg" width = "100px">
+<!--	<img src = "search.jpg" width = "100px">-->
 	<div class = "text">
 	<h1>Search By Name:</h1>
 	<form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 		<input type="text" name="search"></br></br>
 		<div class = "button">
-		<input type="submit" name="submitButton"></br>
+		<input type="submit" name="submitButton"></br></br>
+		<div class="recipe">
+		<?php
+			if(ISSET($_POST['submitButton'])) {
+				while($row = mysqli_fetch_array($result)) {
+					if(strpos(strtolower($row['name']), strtolower($search)) !== false) {
+						echo "<h2>" . $row['name'] . "</h2> ".$row['ingredients'] . " </br>". $row['instructions'];
+						echo "</br>Price: " . $row['price'];
+					}
+				}
+			}
+		?></div>
 		</div>
 	</form>
-<?php
-if(ISSET($_POST['submitButton'])) {
-	while($row = mysqli_fetch_array($result)) {
-		if(strpos(strtolower($row['name']), strtolower($search)) !== false) {
-			echo "<h2>" . $row['name'] . "</h2> ".$row['ingredients'] . " </br>". $row['instructions'];
-			echo "</br>Price: " . $row['price'];
-		}
-	}
-}
-?>
 </div>
 </div>
 </body>
