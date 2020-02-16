ae5b9ec4cca43f8bafa33ee6b7126ba38f020900
kat9897 <61071011+kat9897@users.noreply.github.com>
2020-02-16 03:59:30 -0500

    Add files via upload

---
+++ search.php
@@ -0,0 +1,32 @@
+<?php
+
+if(ISSET($_POST['submitButton'])) {
+	$search = $_POST['search'];
+	
+	require('dbc.php');
+	$query = "SELECT * FROM `recipes`";
+	$result = mysqli_query($dbc, $query) or DIE("Bad connection to recipes.");
+?>
+<html>
+<head>
+	<link rel="stylesheet" href="main.css">
+</head>
+<body>
+<h1>Search By Name:</h1>
+<form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+	<input type="text" name="search"></br></br>
+	<input type="submit" name="submitButton"></br>
+</form>
+<?php
+	while($row = mysqli_fetch_array($result)) {
+		if(strpos(strtolower($row['name']), strtolower($search)) !== false) {
+			echo "<h2>" . $row['name'] . "</h2> ".$row['ingredients'] . " </br>". $row['instructions'];
+			echo "</br>Price: " . $row['price'];
+		}
+
+
+	}
+}
+?>
+</body>
+</html>
